761e912ec08362ce058b0a78d9dca1459b883f89
ked500 <ked500@tut.by>
2022-05-17 15:59:56 +0300

    added mailing to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,25 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+
+  config.action_mailer.delivery_method = :smpt
+  host = 'https://ked500.herokuapp.com'
+  config.action_mailer.default_url_options = {host: host}
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: 'kedruby@gmail.com',
+    password: 'smqdnfibxcjcxtlb',
+    authentication: "plain",
+    :enable_starttls_auto => true
+  }
+  config.action_mailer.perform_caching = false
+
+
+
+
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
