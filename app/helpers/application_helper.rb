0dbcbd663bc8977025001253bb44316c5ae8dfea
ked500 <ked500@tut.by>
2022-05-17 14:45:15 +0300

    added title provider

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_tile = '')
+    base_title = "Simple Blog"
+    if page_tile.empty?
+      base_title
+    else
+      page_tile + " | " + base_title
+    end
+  end
 end
