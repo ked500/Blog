18601d38dfb92bb3482a45af72b4b7a251c50429
ked500 <ked500@tut.by>
2023-02-17 19:34:36 +0300

    Catching error when user is trying to login with external services again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  rescue_from ActionController::InvalidAuthenticityToken, with: :omniauth_failure
 
   protected
   def configure_permitted_parameters
@@ -12,5 +13,13 @@ class ApplicationController < ActionController::Base
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def omniauth_failure
+    if user_signed_in?
+      redirect_to root_path, alert: "You are already signed in."
+    else
+      super
+    end
   end
 end
