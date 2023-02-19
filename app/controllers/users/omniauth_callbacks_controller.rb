527414c662226c0818d91f381dd2c7ab9bd23a7d
ked500 <ked500@tut.by>
2023-02-19 15:45:16 +0300

    Removed console output

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,15 +13,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def handle_auth(kind)
     @user = User.from_omniauth(request.env['omniauth.auth'])
     if @user.persisted?
-      puts "SUCCESS"
       flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: kind
       sign_in_and_redirect @user, event: :authentication
     else
-      puts "FAILURE"
       session['devise.auth_data'] = request.env['omniauth.auth'].except('extra')
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
   end
-
 end
 
