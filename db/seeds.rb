561d29ecf181abc65352039d6cbcdcc790fe5ef5
ked500 <ked500@tut.by>
2023-04-27 02:15:31 +0300

    Added seeds for likes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-
+#
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -20,17 +20,24 @@ User.create!(name:  "Example User",
                confirmed_at: Time.zone.now)
 end
 
-users = User.all
-user = users.first
+ users = User.all
+ user = users.first
 following = users[2..50]
 followers = users[3..40]
-following.each { |followed| user.follow(followed) }
+following.each { |followed| user&.follow(followed) }
 followers.each { |follower| follower.follow(user) }
 
 users = User.order(:created_at).take(6)
 
 50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content)}
-  end
+  content = Faker::Lorem.sentence(word_count: 10)
+  users.each { |_user| _user.microposts.create!(content: content)}
+end
+
+users.each do |_user|
+  number_of_users = rand(2..15)
+  other_users = users.reject { |u| u == _user}.sample(number_of_users)
+  other_users.each{ |other_user| _user.microposts.each { |micropost| micropost.likes.create!(user_id: other_user.id)}}
+end
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
